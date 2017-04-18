a0e165fa0921583ec199b4315cde576ef3740da9
JamesBrofos <jamesbrofos@gmail.com>
2017-04-17 21:02:26 -0400

    remove python cache

--- thor_client/base_url.py
+++ thor_client/base_url.py
@@ -1,2 +1,2 @@
-base_url = "http://127.0.0.1:5000/api/{}/"
-# base_url = "http://aa-lnx01.mitre.org:5000/api/{}/"
+# base_url = "http://127.0.0.1:5000/api/{}/"
+base_url = "http://aa-lnx01.mitre.org:5000/api/{}/"
