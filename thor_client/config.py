9256e934636510aaf02fe694f8e75028b0cb56d7
James Brofos <james.a.brofos.15@dartmouth.edu>
2018-03-19 16:00:18 -0400

    Update config.py

--- thor_client/config.py
+++ thor_client/config.py
@@ -6,5 +6,5 @@ config = configparser.ConfigParser()
 config.read(['config.ini', os.path.expanduser('~/.thorconfig.ini')],
             encoding='utf8')
 
-base_url = os.environ.get('THOR_BASE_URL', config['thor']['base_url'])
-auth_token = os.environ.get('THOR_AUTH_TOKEN', config['thor']['auth_token'])
+base_url = str(os.environ.get('THOR_BASE_URL', config['thor']['base_url']))
+auth_token = str(os.environ.get('THOR_AUTH_TOKEN', config['thor']['auth_token']))
